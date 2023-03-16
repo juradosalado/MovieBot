d8405d7d240d8a6b5690f11a8db66a1b0ccce16b
Tomas Camero <tcamerob@gmail.com>
2023-03-16 19:11:06 +0100

    Create movie, genre, actor and usersession models and csv reader with populatedb

--- MovieBot/main/views.py
+++ MovieBot/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from main.models import Actor, UserSession
+from main.populateDB import populate
 
-# Create your views here.
+
+def populate_database(request):
+    (m, g, a)=populate()
+    message = 'It has been loaded ' + str(m) + ' movies; ' + str(g) + ' genres; ' + str(a) + ' actors.'
+    return render(request, 'base_POPULATEDB.html', {'title': 'End of database load', 'message':message})
+
+def index(request):
+    #DELETE ALL UserSession with more than 24 hours since their date_created:
+    return render(request, 'base_INDEX.html')
